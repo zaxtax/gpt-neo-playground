2511f5387d294fda7e35db77d27991073aca3add
Rob Zinkov <rob@zinkov.com>
2022-12-05 10:22:30 -0500

    Add device selection

--- src/demo.py
+++ src/demo.py
@@ -2,12 +2,18 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 models = ["Salesforce/codegen-2B-mono", "facebook/incoder-1B", "EleutherAI/gpt-neo-2.7B", "EleutherAI/gpt-neo-125M"]
 
+# Only load these models on a machine with lots of VRAM and disc space
+big_models = ["Salesforce/codegen-6B-mono", "Salesforce/codegen-16B-mono", "facebook/incoder-6B"]
+
 tokenizer = AutoTokenizer.from_pretrained(models[0])
 model = AutoModelForCausalLM.from_pretrained(models[0])
 
 def inference(prompt, temperature=0.2, top_p=0.95, max_length=128, device="cuda"):
+    if not torch.cuda.is_available():
+        device = "cpu"
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-    # input_ids = input_ids.to(device)
+    input_ids = input_ids.to(device)
+    model.to(device)
     gen_tokens = model.generate(
         input_ids,
         do_sample=True,
